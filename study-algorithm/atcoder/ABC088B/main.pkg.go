ed9fb15c63e52fa0abb93dd059facebd8d1608fd
Kanai Masumi <masumi.net@gmail.com>
2019-04-07 23:00:30 +0900

    Remove extra brackets

--- study-algorithm/atcoder/ABC088B/main.pkg.go
+++ study-algorithm/atcoder/ABC088B/main.pkg.go
@@ -32,7 +32,7 @@ func (h *intHeap) Push(x interface{}) {
 	h.IntSlice = append(h.IntSlice, x.(int))
 }
 func (h *intHeap) Pop() interface{} {
-	item := ([]int)(h.IntSlice)[len(h.IntSlice)-1]
-	h.IntSlice = ([]int)(h.IntSlice)[0 : len(h.IntSlice)-1]
+	item := []int(h.IntSlice)[len(h.IntSlice)-1]
+	h.IntSlice = []int(h.IntSlice)[0 : len(h.IntSlice)-1]
 	return &item
 }
