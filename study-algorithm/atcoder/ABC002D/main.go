f0edfecf7f377aa3710dd7ec06d9f58dc32b0270
Kanai Masumi <masumi.net@gmail.com>
2019-04-08 22:40:01 +0900

    Refactore ABC022D

--- study-algorithm/atcoder/ABC002D/main.go
+++ study-algorithm/atcoder/ABC002D/main.go
@@ -8,22 +8,22 @@ import (
 func main() {
 	var n, m int
 	fmt.Scan(&n, &m)
-	edgeds := map[int]map[int]struct{}{}
+	edges := map[int]map[int]struct{}{}
 	for i := 1; i <= n; i++ {
-		edgeds[i] = map[int]struct{}{}
+		edges[i] = map[int]struct{}{}
 	}
 	ranks := make([]int, n)
 	for i := 0; i < m; i++ {
 		var x, y int
 		fmt.Scan(&x, &y)
-		edgeds[x][y] = struct{}{}
-		edgeds[y][x] = struct{}{}
+		edges[x][y] = struct{}{}
+		edges[y][x] = struct{}{}
+		// for search upper limit of clique set
 		ranks[x-1]++
 		ranks[y-1]++
 	}
-	// for cutting branch
 	sort.Sort(sort.Reverse(sort.IntSlice(ranks)))
-	limitSize := 1
+	var limitSize int
 	for i := 0; i < n; i++ {
 		if ranks[i] < i {
 			break
@@ -31,24 +31,23 @@ func main() {
 		limitSize = i + 1
 	}
 	// generate k-subset and check it's clique.
-	for i := limitSize; i > 0; i-- {
-		if searchClique(i, 0, nil, edgeds) {
-			fmt.Printf("%d\n", i)
+	for k := limitSize; k > 0; k-- {
+		if searchKClique(k, 0, n, nil, edges) {
+			fmt.Printf("%d\n", k)
 			return
 		}
 	}
 }
 
-func searchClique(size, min int, nodes []int, edgeds map[int]map[int]struct{}) bool {
-	n := len(edgeds)
-	if min == n+1 || size == 0 {
-		return size == 0
+func searchKClique(k, idx, last int, nodes []int, edges map[int]map[int]struct{}) bool {
+	if idx == last+1 || k == 0 {
+		return k == 0
 	}
-	for i := min; i <= (n+1)-size; i++ {
-		if searchClique(size-1, i+1, append(nodes, i), edgeds) {
+	for i := idx; i <= last-k+1; i++ {
+		if searchKClique(k-1, i+1, last, append(nodes, i), edges) {
 			allConnected := true
 			for _, j := range nodes {
-				if _, ok := edgeds[i][j]; !ok {
+				if _, ok := edges[i][j]; !ok {
 					allConnected = false
 					break
 				}
@@ -57,7 +56,7 @@ func searchClique(size, min int, nodes []int, edgeds map[int]map[int]struct{}) b
 				return true
 			}
 		}
-		if searchClique(size, i+1, nodes, edgeds) {
+		if searchKClique(k, i+1, last, nodes, edges) {
 			return true
 		}
 	}
