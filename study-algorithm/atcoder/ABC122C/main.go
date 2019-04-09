d5a1499dc631d3ec49100bb34318162bc7ae5742
Kanai Masumi <masumi.net@gmail.com>
2019-04-09 13:36:18 +0900

    Make ABC122C AC

--- study-algorithm/atcoder/ABC122C/main.go
+++ study-algorithm/atcoder/ABC122C/main.go
@@ -1,56 +1,45 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
 	var n, q int
 	fmt.Scan(&n, &q)
-	s := scanLine()
+	var s string
+	fmt.Scan(&s)
 	culumtive := make([]int, n)
-	m, num := &acDFA{}, 0
-	for i := 0; i < n; i++ {
-		if m.recognize(s[i]) {
+	num := 0
+	for i := 1; i < n; i++ {
+		if s[i] == 'C' && s[i-1] == 'A' {
 			num++
 		}
 		culumtive[i] = num
 	}
 	var l, r int
-	ans := make([]int, q)
 	for i := 0; i < q; i++ {
-		fmt.Scan(&l)
-		fmt.Scan(&r)
-		ans[i] = culumtive[r-1] - culumtive[l-1]
-	}
-	for _, a := range ans {
+		fmt.Scan(&l, &r)
+		a := culumtive[r-1] - culumtive[l-1]
 		fmt.Printf("%d\n", a)
 	}
 }
 
-type state int
-type acDFA struct {
-	s state
-}
+// type acDFA struct{ s byte }
+//
+// func (m *acDFA) recognize(c byte) bool {
+// 	if m.s == 'A' && c == 'C' {
+// 		m.s = 'C'
+// 		return true
+// 	}
+// 	m.s = c
+// 	return false
+// }
 
-func (m *acDFA) recognize(c byte) bool {
-	if c == 'A' {
-		m.s = 1
-		return false
-	}
-	if m.s == 1 && c == 'C' {
-		return true
-	}
-	m.s = 0
-	return false
-}
-
-func scanLine() (s string) {
-	sc := bufio.NewScanner(os.Stdin)
-	size := 100000
-	sc.Buffer(make([]byte, size), size)
-	sc.Scan()
-	return sc.Text()
-}
+// func scanLine() (s string) {
+// 	sc := bufio.NewScanner(os.Stdin)
+// 	size := 100000
+// 	sc.Buffer(make([]byte, size), size)
+// 	sc.Scan()
+// 	return sc.Text()
+// }
