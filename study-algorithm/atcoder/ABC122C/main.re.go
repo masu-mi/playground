bdca2791a15e8b03fb7967386ecfbd2815a9e906
Kanai Masumi <masumi.net@gmail.com>
2019-04-10 10:23:01 +0900

    Update ABC122C

--- study-algorithm/atcoder/ABC122C/main.re.go
+++ study-algorithm/atcoder/ABC122C/main.re.go
@@ -10,9 +10,9 @@ func main() {
 	var s string
 	fmt.Scan(&s)
 	culumtive := make([]int, n)
-	num := 0
-	for i := 1; i < n; i++ {
-		if s[i] == 'C' && s[i-1] == 'A' {
+	num, m := 0, &dfa{}
+	for i := 0; i < n; i++ {
+		if m.recognize(s[i]) {
 			num++
 		}
 		culumtive[i] = num
@@ -23,4 +23,15 @@ func main() {
 		a := culumtive[r-1] - culumtive[l-1]
 		fmt.Printf("%d\n", a)
 	}
+}
+
+type dfa struct{ s byte }
+
+func (m *dfa) recognize(c byte) bool {
+	if m.s == 'A' && c == 'C' {
+		m.s = 'C'
+		return true
+	}
+	m.s = c
+	return false
 }
