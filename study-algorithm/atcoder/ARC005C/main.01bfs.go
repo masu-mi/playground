cd16212baf4c68aeea7e9cef1683747fb9103a79
Kanai Masumi <masumi.net@gmail.com>
2019-04-14 18:10:24 +0900

    Resolve ARC005C with 0-1-BFS

--- study-algorithm/atcoder/ARC005C/main.01bfs.go
+++ study-algorithm/atcoder/ARC005C/main.01bfs.go
@@ -3,18 +3,56 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"math"
 )
 
 func main() {
 	var h, w int
 	fmt.Scan(&h, &w)
 	s, g, grid := loadGrid(h, w)
-	broken := bfsMinNumOfBrokenWall(h, w, s, g, grid)
-	if broken <= 2 {
+	if bfsReachableWithBreak(h, w, 2, s, g, grid) {
 		fmt.Println("YES")
 	} else {
 		fmt.Println("NO")
 	}
+}
+
+func bfsReachableWithBreak(h, w, limit int, s, g pos, grid []string) bool {
+	visited := make([][]bool, h+2)
+	for i := 0; i <= h+1; i++ {
+		visited[i] = make([]bool, w+2)
+	}
+
+	q := newDeque(h * w)
+	q.PushBack(candidate{s, -math.MaxInt32})
+	for !q.Empty() {
+		cur := q.Pop() // cur := q[0]
+		visited[cur.p.y][cur.p.x] = true
+		if eq(cur.p, g) {
+			return cur.c+math.MaxInt32 <= limit
+		}
+		for _, d := range []pos{{0, 1}, {0, -1}, {1, 0}, {-1, 0}} {
+			next := pos{cur.p.y + d.y, cur.p.x + d.x}
+			if !(next.y >= 1 && next.y <= h && next.x >= 1 && next.x <= w) {
+				continue
+			}
+			if visited[next.y][next.x] {
+				continue
+			}
+			if grid[next.y][next.x] == '#' {
+				if cur.c+1+math.MaxInt32 > limit {
+					continue
+				}
+				q.PushBack(candidate{next, cur.c + 1})
+			} else {
+				if cur.c+math.MaxInt32 > limit {
+					continue
+				}
+				q.PushFront(candidate{next, cur.c})
+			}
+		}
+	}
+	return false
 }
 
 type pos struct{ y, x int }
@@ -24,12 +62,6 @@ func eq(a, b pos) bool { return a.y == b.y && a.x == b.x }
 type candidate struct {
 	p pos
 	c int
-}
-
-func reverse(cs []candidate) {
-	for i := 0; i < len(cs)/2; i++ {
-		cs[i], cs[len(cs)-i-1] = cs[len(cs)-i-1], cs[i]
-	}
 }
 
 type deque struct {
@@ -49,11 +81,9 @@ func (dq *deque) Empty() bool {
 func (dq *deque) PushBack(c candidate) {
 	dq.rq = append(dq.rq, c)
 }
-
 func (dq *deque) PushFront(c candidate) {
 	dq.lq = append(dq.lq, c)
 }
-
 func (dq *deque) Pop() (c candidate) {
 	if len(dq.lq) > 0 {
 		c = dq.lq[len(dq.lq)-1]
@@ -63,37 +93,6 @@ func (dq *deque) Pop() (c candidate) {
 	c = dq.rq[0]
 	dq.rq = dq.rq[1:len(dq.rq)]
 	return c
-}
-
-func bfsMinNumOfBrokenWall(h, w int, s, g pos, grid []string) int {
-	visited := make([][]bool, h+2)
-	for i := 0; i <= h+1; i++ {
-		visited[i] = make([]bool, w+2)
-	}
-	q := newDeque(h * w)
-	q.PushBack(candidate{s, 0})
-	for !q.Empty() {
-		cur := q.Pop() // cur := q[0]
-		visited[cur.p.y][cur.p.x] = true
-		if eq(cur.p, g) {
-			return cur.c
-		}
-		for _, d := range []pos{{0, 1}, {0, -1}, {1, 0}, {-1, 0}} {
-			next := pos{cur.p.y + d.y, cur.p.x + d.x}
-			if !(next.y >= 1 && next.y <= h && next.x >= 1 && next.x <= w) {
-				continue
-			}
-			if visited[next.y][next.x] {
-				continue
-			}
-			if grid[next.y][next.x] == '#' {
-				q.PushBack(candidate{next, cur.c + 1})
-			} else {
-				q.PushFront(candidate{next, cur.c})
-			}
-		}
-	}
-	return -1
 }
 
 func wall(w int) string {
