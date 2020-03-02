33e275f67307347c6dbb33aed6ec866c8de10e7a
Kanai Masumi <masumi.net@gmail.com>
2020-03-03 08:03:04 +0900

    Bugfix ABC147C

--- study-algorithm/answers/atcoder/ABC147C/main.go
+++ study-algorithm/answers/atcoder/ABC147C/main.go
@@ -13,21 +13,51 @@ func main() {
 	fmt.Printf("%d\n", resolve(parseProblem(os.Stdin)))
 }
 
-func parseProblem(r io.Reader) int {
+func parseProblem(r io.Reader) (int, []testimony) {
 	var n int
 	fmt.Fscanf(r, "%d", &n)
+
 	sc := bufio.NewScanner(r)
-	sc.Split(bufio.ScanWords)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+
 	testimonies := []testimony{}
 	for i := 0; i < n; i++ {
 		testimonies = append(testimonies, parseTestimony(sc))
 	}
+	return n, testimonies
+}
+
+type testimony struct{ h, l uint32 }
+
+func parseTestimony(sc *bufio.Scanner) (t testimony) {
+	n := scanInt(sc)
+
+	t.l, t.h = math.MaxUint32, 0
+	for i := 0; i < n; i++ {
+		idx := scanInt(sc)
+		horneset := scanInt(sc)
+		if horneset == 0 {
+			t.l ^= 1 << uint(idx-1)
+		} else {
+			t.h |= 1 << uint(idx-1)
+		}
+	}
+	return
+}
+
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+
+func resolve(n int, testimonies []testimony) int {
 	max := uint32(0)
 	for i := 0; i < 1<<uint32(n); i++ {
 		if checkAllHornest(n, uint32(i), testimonies) {
-			numH := onesCount(uint32(i))
-			if max < numH {
-				max = numH
+			numH := onesCount(uint64(i))
+			if max < uint32(numH) {
+				max = uint32(numH)
 			}
 		}
 	}
@@ -52,34 +82,17 @@ func checkAllHornest(n int, i uint32, testimonies []testimony) bool {
 	return true
 }
 
-func onesCount(bits uint32) (num uint32) {
-	num = (bits >> 1) & 03333333333
-	num = bits - num - ((num >> 1) & 03333333333)
-	num = ((num + (num >> 3)) & 0707070707) % 077
-	return
-}
+func onesCount(x uint64) (num int) {
+	const m0 = 0x5555555555555555 // 01010101 ...
+	const m1 = 0x3333333333333333 // 00110011 ...
+	const m2 = 0x0f0f0f0f0f0f0f0f // 00001111 ...
 
-type testimony struct{ h, l uint32 }
-
-func parseTestimony(sc *bufio.Scanner) (t testimony) {
-	sc.Scan()
-	n, _ := strconv.Atoi(sc.Text())
-
-	t.l, t.h = math.MaxUint32, 0
-	for i := 0; i < n; i++ {
-		sc.Scan()
-		idx, _ := strconv.Atoi(sc.Text())
-		sc.Scan()
-		horneset, _ := strconv.Atoi(sc.Text())
-		if horneset == 0 {
-			t.l ^= 1 << uint32(idx-1)
-		} else {
-			t.h |= 1 << uint32(idx-1)
-		}
-	}
-	return
-}
-
-func resolve(n int) int {
-	return n
+	const m = 1<<64 - 1
+	x = x>>1&(m0&m) + x&(m0&m)
+	x = x>>2&(m1&m) + x&(m1&m)
+	x = (x>>4 + x) & (m2 & m)
+	x += x >> 8
+	x += x >> 16
+	x += x >> 32
+	return int(x) & (1<<7 - 1)
 }
