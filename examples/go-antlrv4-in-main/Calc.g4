483c08c1f1a8c4f77426fbef6129609fcbe8cf94
Kanai Masumi <masumi.net@gmail.com>
2020-04-18 22:24:51 +0900

    Add action for study

--- examples/go-antlrv4-in-main/Calc.g4
+++ examples/go-antlrv4-in-main/Calc.g4
@@ -11,7 +11,7 @@ grammar Calc;
 prog:   expr NEWLINE*;
 expr:   '(' expr_=expr ')'
     |   left=expr op=('*'|'/') right=expr
-    |   left=expr op=('+'|'-') right=expr
+    |   left=expr { fmt.Printf("%v\n", $left.text) } op=('+'|'-') right=expr { fmt.Printf("ctx: %v, start: %v, left: %v, right_start_token: %v\n", $ctx, $start, $left.text, $right.start) }
     |   atom=INT
     ;
 
--- examples/go-antlrv4-in-main/main.go
+++ examples/go-antlrv4-in-main/main.go
@@ -7,12 +7,11 @@ import (
 )
 
 func main() {
-	input := antlr.NewInputStream("12+3*4") // ← 入力
-
+	input := antlr.NewInputStream("(12+3+8)*4")
 	lexer := NewCalcLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, 0)
 	p := NewCalcParser(stream)
-	tree := p.Prog() // ← ルールのトップに対応する関数が用意されてるのでそれを起動すると解析して解析木を返す
+	tree := p.Prog()
 
 	fmt.Println(tree.ToStringTree([]string{}, p)) // ← 解析木を LISP-style で表示する，最初の文字列配列をなぜ与えるのかはよく分からん
 }
