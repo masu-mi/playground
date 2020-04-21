eea2e253064b5e7f40d17599d034a6f03958521e
Kanai Masumi <masumi.net@gmail.com>
2020-04-22 07:51:51 +0900

    'Add directory "ABC120B" with addir.sh'

--- study-algorithm/answers/atcoder/ABC120B/main.go
+++ study-algorithm/answers/atcoder/ABC120B/main.go
@@ -1,17 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
 
 func main() {
-	var a, b, k int
-	fmt.Scanf("%d %d %d", &a, &b, &k)
-	fmt.Printf("%d\n", findInt(a, b, k))
+	fmt.Printf("%d\n", resolve(parseProblem(os.Stdin)))
 }
 
-func findInt(a, b, k int) int {
-	lim := min(a, b)
+func parseProblem(r io.Reader) (int, int, int) {
+	const (
+		initialBufSize = 100000
+		maxBufSize     = 1000000
+	)
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	a, b, k := scanInt(sc), scanInt(sc), scanInt(sc)
+	return a, b, k
+}
+
+func resolve(a, b, k int) int {
+	max := min(a, b)
 	count := 0
-	for i := lim; i >= 1; i-- {
+	for i := max; i > 0; i-- {
 		if a%i == 0 && b%i == 0 {
 			count++
 		}
@@ -19,12 +37,23 @@ func findInt(a, b, k int) int {
 			return i
 		}
 	}
+	// out of scope
 	return -1
 }
-
 func min(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
 }
