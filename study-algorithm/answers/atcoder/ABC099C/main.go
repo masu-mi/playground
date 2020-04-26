2af88a7346f420c1e6bd635d160d38aa8d409986
Kanai Masumi <masumi.net@gmail.com>
2020-04-27 08:00:51 +0900

    'Add directory "ABC099C" with addir.sh'

---
+++ study-algorithm/answers/atcoder/ABC099C/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Printf("%d\n", resolve(parseProblem(os.Stdin)))
+}
+
+func parseProblem(r io.Reader) int {
+	const (
+		initialBufSize = 100000
+		maxBufSize     = 1000000
+	)
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+
+	return scanInt(sc)
+}
+
+func resolve(n int) int {
+	// index == value
+	states := make([]int, n+1)
+	for i := 1; i < n+1; i++ {
+		states[i] = inf
+	}
+	// 配るDP
+	for i := 0; i < n; i++ {
+		if i+1 <= n {
+			changeToMin(&(states[i+1]), states[i]+1)
+		}
+		for idx := 6; i+idx <= n; idx *= 6 {
+			changeToMin(&(states[i+idx]), states[i]+1)
+		}
+		for idx := 9; i+idx <= n; idx *= 9 {
+			changeToMin(&(states[i+idx]), states[i]+1)
+		}
+	}
+	return states[n]
+}
+
+const (
+	inf  = math.MaxInt32
+	ninf = math.MinInt32
+)
+
+func changeToMin(v *int, cand int) (updated bool) {
+	if *v > cand {
+		*v = cand
+		updated = true
+	}
+	return updated
+}
+
+func changeToMax(v *int, cand int) (updated bool) {
+	if *v < cand {
+		*v = cand
+		updated = true
+	}
+	return updated
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
