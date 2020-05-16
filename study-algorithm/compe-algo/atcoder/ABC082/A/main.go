bb3fae1bcdc55cc34cbd8012f23c70d4a99e1e5b
Kanai Masumi <masumi.net@gmail.com>
2020-05-16 22:53:57 +0900

    Remove study-algorithm

--- study-algorithm/compe-algo/atcoder/ABC082/A/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var a, b int
-	fmt.Scan(&a, &b)
-	fmt.Printf("%d\n", (a+b+1)/2)
-}
--- study-algorithm/compe-algo/atcoder/mkworkspace.sh
+++
@@ -1,8 +0,0 @@
-#!/Users/masumi/local/bin/bash
-
-url=$1
-problem=$(basename $url)
-dir=$(echo ${problem^^} | sed -E 's/_//g')
-
-mkdir $dir
-touch $dir/main.go
