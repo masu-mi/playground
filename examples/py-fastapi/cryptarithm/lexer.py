5d9acfce73c216c935155f9466158142eeda17f6
Kanai Masumi <masumi.net@gmail.com>
2020-07-09 23:12:33 +0900

    Improve lexer

--- examples/py-fastapi/cryptarithm/lexer.py
+++ examples/py-fastapi/cryptarithm/lexer.py
@@ -19,7 +19,23 @@ class Token:
 
     @classmethod
     def tokenize(cls, raw):
-        for token in raw.split(" "):
+        internal = cls._split(raw)
+        for token in internal:
             if token != '':
                 yield Token(token)
-        yield Token('')
+        while True:
+            yield Token('')
+
+    @classmethod
+    def _split(cls, raw):
+        last = -1
+        for i in range(len(raw)):
+            if raw[i] == " ":
+                yield raw[last+1:i]
+                last = i
+                continue
+            if raw[i] in cls.operators:
+                yield raw[last+1:i]
+                yield raw[i]
+                last = i
+        yield raw[last+1:len(raw)]
