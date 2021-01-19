b63560d3d11c3a710b34a8bb7e175cc68cf15542
Kanai Masumi <masumi.net@gmail.com>
2021-01-19 09:20:24 +0900

    Add graceful shutdown

--- training-code-design/cheap-monster-hunter/game/main.go
+++ training-code-design/cheap-monster-hunter/game/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	app "github.com/masu-mi/playground/training-code-design/cheap-monster-hunter/game/adapter/controller/http"
 	"github.com/masu-mi/playground/training-code-design/cheap-monster-hunter/game/adapter/gateway/dummy"
@@ -20,5 +24,28 @@ func main() {
 		MonsterRepo: &dummy.MonsterRepo{},
 	})
 	handler.SetLogger(service.Logger)
-	http.ListenAndServe(":8080", handler)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, syscall.SIGINT, syscall.SIGTERM)
+		<-sigint
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(idleConnsClosed)
+	}()
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+
+	<-idleConnsClosed
 }
